3d7e47f29929537f68c2dff11dc6bb205c8d3b08
wsw0108 <wsw0108@gmail.com>
2017-08-01 19:38:26 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,7 @@
 uname -a
 pwd
 tree $HOME/site-example
+git config --global user.email "deploy@circle-ci.org"
+git config --global user.name "Deployment Bot"
 git -C $HOME/site-example add $HOME/site-example/example1
 git -C $HOME/site-example commit -m "update example1"
