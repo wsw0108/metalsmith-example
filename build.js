2cf10c19ce807d60f344fb141d5d5aacd7a7019d
wsw0108 <wsw0108@gmail.com>
2017-08-01 17:51:40 +0800

    try do deploy

--- build.js
+++ build.js
@@ -3,6 +3,7 @@ const layouts = require('metalsmith-layouts')
 
 Metalsmith(__dirname)
   .use(layouts('handlebars'))
+  .destination('../site-example/example1')
   .build(function(err) {
     if (err) throw err
     console.log('Build finished!')
--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,5 @@
 
 uname -a
 pwd
+git -C $HOME/site-example add $HOME/site-example/example1
+git -C $HOME/site-example commit -m "update example1"
