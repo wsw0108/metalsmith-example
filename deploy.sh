cfd57305b72fb4261a6b6346552ebff007ef5b3d
wsw0108 <wsw0108@gmail.com>
2017-08-01 19:44:03 +0800

    update deplpy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,4 @@ git config --global user.email "deploy@circle-ci.org"
 git config --global user.name "Deployment Bot"
 git -C $HOME/site-example add $HOME/site-example/example1
 git -C $HOME/site-example commit -m "update example1"
+git -C $HOME/site-example push
