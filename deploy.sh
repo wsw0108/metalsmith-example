0263a9b4020ad213cfc24bf87b47779dddf45923
wsw0108 <wsw0108@gmail.com>
2017-08-02 08:57:26 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,20 @@
 
 uname -a
 pwd
+
+git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
+git config --global user.name "$GH_NAME" > /dev/null 2>&1
+
 cd $HOME/site-example
 tree .
-git config --global user.email "$GH_EMAIL"
-git config --global user.name "$GH_NAME"
+
+if git rev-parse --verify origin/gh-pages > /dev/null 2>&1
+then
+    git checkout gh-pages
+else
+    git checkout --orphan gh-pages
+fi
+
 git add example1
 git commit -m "update example1"
 git push --force origin gh-pages
