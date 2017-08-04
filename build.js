d359344b2a1505c9e4b96347acfa16cf07122896
wsw0108 <wsw0108@gmail.com>
2017-08-04 08:52:06 +0800

    a simple localize solution

--- build.js
+++ build.js
@@ -2,6 +2,9 @@ const Metalsmith = require('metalsmith')
 const layouts = require('metalsmith-layouts')
 
 Metalsmith(__dirname)
+  .metadata({
+    locale: 'en'
+  })
   .use(layouts('handlebars'))
   .destination('../site-example/example1')
   .build(function(err) {
