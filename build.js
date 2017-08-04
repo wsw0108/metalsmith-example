d01f1a44d95c91c35a45ff3e5d71aa0e5c2d7be1
wsw0108 <wsw0108@gmail.com>
2017-08-04 09:17:23 +0800

    plugins: rootpath, debug, assets

--- build.js
+++ build.js
@@ -1,12 +1,20 @@
 const Metalsmith = require('metalsmith')
+const rootpath = require('metalsmith-rootpath')
+const assets = require('metalsmith-assets')
 const layouts = require('metalsmith-layouts')
+const debug = require('metalsmith-debug')
 
 Metalsmith(__dirname)
   .metadata({
     locale: 'en'
   })
+  .use(rootpath())
   .use(layouts('handlebars'))
+  .use(assets({
+    source: 'assets'
+  }))
   .destination('../site-example/example1')
+  .use(debug())
   .build(function(err) {
     if (err) throw err
     console.log('Build finished!')
