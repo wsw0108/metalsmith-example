13570d6a7d0ae96dd74e7235cf20d5b4fdc18f0e
wsw0108 <wsw0108@gmail.com>
2017-08-15 07:43:33 +0800

    i18n

--- build.js
+++ build.js
@@ -1,15 +1,19 @@
+const path = require('path')
 const Metalsmith = require('metalsmith')
+const i18n = require('metalsmith-i18next')
 const rootpath = require('metalsmith-rootpath')
 const assets = require('metalsmith-assets')
 const layouts = require('metalsmith-layouts')
 const debug = require('metalsmith-debug')
 
 Metalsmith(__dirname)
+  .use(i18n({
+    path: ':locale' + path.sep + ':file',
+    locales: ['en', 'zh']
+  }))
   .use(rootpath())
   .use(layouts('handlebars'))
-  .use(assets({
-    source: 'assets'
-  }))
+  .use(assets())
   .use(debug())
   .build(function(err) {
     if (err) throw err
