9ead68c4e3b24206dcec2f775758a03ace0ed729
wsw0108 <wsw0108@gmail.com>
2017-08-14 21:44:13 +0800

    simple

--- build.js
+++ build.js
@@ -5,15 +5,11 @@ const layouts = require('metalsmith-layouts')
 const debug = require('metalsmith-debug')
 
 Metalsmith(__dirname)
-  .metadata({
-    locale: 'en'
-  })
   .use(rootpath())
   .use(layouts('handlebars'))
   .use(assets({
     source: 'assets'
   }))
-  .destination('../site-example/example1')
   .use(debug())
   .build(function(err) {
     if (err) throw err
